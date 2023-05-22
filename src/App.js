31133d9232be2e10e32840ae5ac3f185bbfb7b3f
munisekhar1434 <kuntumallamunisekhar2000@gmail.com>
2023-05-22 09:53:37 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import TextEditor from "./components/TextEditor";
 
@@ -7,7 +6,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1>React Text Editor</h1>
       </header>
       <div className="editor">
